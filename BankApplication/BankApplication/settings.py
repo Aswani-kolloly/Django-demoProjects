d858f57180975186df91ba523bc5aae8bc620443
Aswani-kolloly <77399867+Aswani-kolloly@users.noreply.github.com>
2021-05-17 17:31:42 +0530

    Update settings.py

--- BankApplication/BankApplication/settings.py
+++ BankApplication/BankApplication/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kol3u17xl%_@#=)q-%d6oj1wt2u+io5#(g_-bkoe@j32=j9c5n'
+SECRET_KEY=''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
